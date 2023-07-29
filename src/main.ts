c856e5c3a9faab31cd67fae506c8202e55c77303
Jadson Souza <jadsonsouza@Jadsons-MacBook-Pro.local>
2023-07-29 20:13:30 -0300

    feat: add prisma error handler

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Auth
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new AuthGuard(reflector));
-  
+
+  // Prisma Error Handdler
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3001);
   app.enableShutdownHooks();
 }
